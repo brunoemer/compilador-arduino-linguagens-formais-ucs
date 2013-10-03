ac8439f00b0f12175ea791102abe048200619f5f
glaubercini@gmail.com <glaubercini@gmail.com@d20387fb-2cd3-a906-eb1c-bf456215fd21>
2013-10-03 16:35:24 +0000

    Criando repositório usando o http://ankhsvn.open.collab.net/

---
+++ LinguagensFormais/LinguagensFormais/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompiladoresTrabalho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool debug = true;
+
+            Console.WriteLine("Iniciado processo...");
+
+            try
+            {
+                AnalisadorSintatico analisadorSintatico = new AnalisadorSintatico();
+                String codigo = analisadorSintatico.Execute();
+
+                StringBuilder sb = new StringBuilder();
+                foreach (string item in LineManager.Instance.LinesOut)
+                {
+                    sb.Append(item);
+                }
+
+                if (debug)
+                {
+                    Console.WriteLine(codigo);
+                    Console.Write(sb.ToString());
+                    Console.WriteLine(TS.Instance.ToString());
+                }
+
+                Arquivo.Write(@"Arquivos/codigo.c", codigo);
+                Arquivo.Write(@"Arquivos/saida_lex.txt", sb.ToString());
+                Arquivo.Write(@"Arquivos/saida_ts.txt", TS.Instance.ToString());
+            }
+            catch (AnalisadorException ae)
+            {
+                Console.WriteLine(ae.Message);
+                Console.WriteLine();
+                Console.WriteLine("---ESTADO---");
+                Console.WriteLine();
+                Console.WriteLine(String.Format("Posição vetor da string: {0}", ae.Pos));
+                Console.WriteLine(String.Format("Posição coluna: {0}", ae.Col));
+                Console.WriteLine(String.Format("Linha: {0}", ae.Linha));
+                Console.WriteLine(String.Format("Token: {0}", ae.Token));
+                Console.WriteLine(String.Format("Código do Token: {0}", ae.TokenCod));
+                Console.WriteLine();
+                Console.WriteLine("------------");
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("---MEMORIA---");
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                Console.WriteLine("-------------");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Processo concluído, precione uma tecla...");
+            Console.ReadKey();
+        }
+    }
+}
